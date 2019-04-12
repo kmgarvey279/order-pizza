94f1ebaec0f182426f941120ee775f50891b4500
Kevin Garvey <kmgarvey279@gmail.com>
2019-04-12 10:25:01 -0700

    Add user interface logic to take input and display pizzas

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,44 @@
+// Business Logic
+
+//Order Object Logic
+function Order() {
+  this.pizzas = [];
+}
+
+Order.prototype.addPizza = function(pizza) {
+  this.pizzas.push(pizza);
+}
+
+Order.prototype.getTotalPrice = function() {
+  this.pizzas.push(pizza);
+}
+
+
+//Pizza Object Logic
+function Pizza(size, crust) {
+  this.size = size;
+  this.crust = crust;
+  this.toppings = [];
+}
+
+Pizza.prototype.addTopping = function(topping) {
+  this.toppings.push(topping);
+}
+
+// Pizza.prototype.getPizzaPrice() = function() {
+//   this.
+// }
+
+
+// Front End Logic
+var newOrder= new Order();
+
+function displayOrder(orderToDisplay) {
+  orderToDisplay.pizzas.forEach(function(pizza) {
+    $("#my-pizza-list").append(pizza.size + pizza.crust)
+  })
+}
+
 $(document).ready(function() {
   $("form#pizza-input").submit(function(event) {
     event.preventDefault();
@@ -5,9 +46,12 @@ $(document).ready(function() {
     var pizzaSize = $("#input-size").val();
     var pizzaCrust = $("#input-crust").val();
     var toppingsArr = []
-    $('input:checkbox[name=type]:checked').each(function() {
-      toppingsArr.push($(this).val());
-      console.log(toppingsArr);
-    })
+    var newPizza = new Pizza(pizzaSize, pizzaCrust);
+    newOrder.addPizza(newPizza);
+    displayOrder(newOrder);
+    // $('input:checkbox[name=type]:checked').each(function() {
+    //   toppingsArr.push($(this).val());
+    //   console.log(toppingsArr);
+    // })
   })
 });
